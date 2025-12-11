4dda589111828b685734a12323cbd363e0e39cb0
azhuravlev <zhuravlev.a.a@me.com>
2025-12-11 22:39:39 +0300

    added widget component & refactored api url

--- shared/api/quiz/types.ts
+++ shared/api/quiz/types.ts
@@ -1,9 +1,10 @@
 export interface ApiQuizResponse {
-  bgColor: string; // HEX
+  bgColor: string;
+  bannerBgColor: string;
   description: string;
   entityId: number;
   image: string; // base64
-  imageSmall: string; // base64
+  widgetImage: string; // base64
   shortDescription: string;
   tasks: ApiTaskResponse[];
   title: string;
